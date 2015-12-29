937d5434c40849aacfa8225989c228ae0e04e3bc
vakulchyk <e.vakulchyk@gmail.com>
2015-12-29 20:34:00 +0700

    firdst commit

---
+++ jquery.js
@@ -0,0 +1,13 @@
+
+// if (datefield.type!="date"){ //if browser doesn't support input type="date", initialize date picker widget:
+//     jQuery(function($){ //on document.ready
+//         $('#datepic').datepicker();
+//     })
+// }
+
+$(document).ready(function(){
+    $("input[type=date]").click(function(){
+        $("input[type=date]").addClass("addedDate");
+    })   
+})
+
