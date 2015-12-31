dd56a5c963d56a432e60ee9ef995113f00efcba8
vakulchyk <e.vakulchyk@gmail.com>
2015-12-31 14:51:08 +0700

    HTML moved to JQ

--- jquery.js
+++ jquery.js
@@ -1,10 +1,10 @@
-
-
 $(document).ready(function(){
-    // $("input[type=date]").append('<label class="date_picker_label">Select date <span class="mls icon-icn_calendar2"></span></label>');
+    // insert requiered elements for date picker
+    $('<label class="date_picker_label">Select date <span class="mls icon-icn_calendar2"></span></label>').insertAfter($("input[type=date]"));
     $("input[type=date]").click(function(){
         $(this).addClass("addedDate");
     })
+    // modifing view of date picker onclick
     $("input[type=date]").click(function(){
         $(this).next($(".date_picker_label")).css("visibility", "hidden");
         $(this).next($(".date_picker_label")).children().css( "visibility", "visible" );
